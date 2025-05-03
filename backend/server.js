b11729820147387a1febb192aec9ed911f4223ec
swapnilsubhashpatil <patilswapnilsubhash@gmail.com>
2025-05-03 15:23:54 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://bitebook-6n9h.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
